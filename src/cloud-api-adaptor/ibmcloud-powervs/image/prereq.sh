2ed5beb945cd27a554f61dee223acaeb4f05c9a4
Amulyam24 <amulmek1@in.ibm.com>
2025-02-26 15:26:15 +0530

    update dependencies

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -2,6 +2,9 @@
 
 # FIXME to pickup these values from versions.yaml
 GO_VERSION="1.22.12"
+ORAS_VERSION="1.2.0"
+
+export PATH=${PATH}:/usr/local/go/bin:${HOME}/.cargo/bin
 
 # Install dependencies
 yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel
@@ -9,8 +12,11 @@ yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-de
 wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
 yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
 
-wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
-yum install -y device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
+wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.202-2.el9.ppc64le.rpm
+yum install -y device-mapper-devel-1.02.202-2.el9.ppc64le.rpm
+
+wget https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_linux_ppc64le.tar.gz
+rm -rf /usr/local/bin/oras && tar -C /usr/local/bin -xzf oras_${ORAS_VERSION}_linux_ppc64le.tar.gz && rm -f oras_${ORAS_VERSION}_linux_ppc64le.tar.gz
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
